1acb3b5b07931cabd4813515183b2fd17285b075
MALCO ROMEO JR M <malcoromeojr@gmail.com>
2026-08-12 07:30:35 +0800

    Add files via upload

--- Malco Romeo_BSIS 3A/home.php
+++ Malco Romeo_BSIS 3A/home.php
@@ -1,15 +1,25 @@
-<?php
-session_start();
 
-?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <h1>hello world <?php echo  $_SESSION['first_name'];?> </h1>
+    <h1>Welcome to homepage!</h1>
+    <h1><?php echo $_SESSION['firstname'] . $_SESSION['lastname']; ?></h1>
+    <a type="button" href="logout.php">Logout</a>
 </body>
+
 </html>
+
+<?php
+
+'}'else'{'
+    echo "You dont have permission to acccess this!";
+    }
+
+?>
